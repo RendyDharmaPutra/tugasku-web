4bb0c712c20c1f372f40ff1c1599ce2f99232acf
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-07-21 00:31:11 +0700

    feat(home): redirecting to register page from home route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,12 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [{ title: "TugasKu - Home" }];
 };
+
+export async function loader() {
+  return redirect("/register");
+}
 
 export default function Index() {
   return <div>hello</div>;
