c0a8e261784788d31505afa485f5ce2211c53a93
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-07-26 22:35:08 +0700

    fix(verify): fix error on showing icon verify status

--- app/auth/components/verify/verify-header.tsx
+++ app/auth/components/verify/verify-header.tsx
@@ -12,7 +12,11 @@ export const VerifyHeader = ({
   return (
     <div className="flex justify-center">
       <div
-        className={`h-16 w-16 bg-${background} rounded-full flex items-center justify-center`}
+        className={`h-16 w-16 ${
+          background === "primary"
+            ? "bg-primary-accent dark:bg-primary-accent-dark"
+            : "bg-danger dark:bg-danger-dark"
+        } rounded-full flex items-center justify-center`}
       >
         <props.icon
           className={`w-8 h-8 text-primary-background ${
