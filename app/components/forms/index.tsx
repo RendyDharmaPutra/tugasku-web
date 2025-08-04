f5aea246792633d3f531d78cd689ab591ef2deb9
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-08-04 20:59:04 +0700

    fixed(components): fixed error caused exporting unavailable components

--- app/components/forms/index.tsx
+++ app/components/forms/index.tsx
@@ -1,6 +1,4 @@
 export { TextInput } from "./text-input";
 export { PasswordInput } from "./password-input";
-export { SelectInput } from "./select-input";
-export { TimeInput } from "./time-input";
 export { SubmitBtn } from "./submit-btn";
 export * from "./fields";
